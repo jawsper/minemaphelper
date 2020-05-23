ddd3360952b0241110e4ebb9daa9d23bee8daa07
Jasper Seidel <code@jawsper.nl>
2020-05-23 20:09:34 +0200

    [backend] Added finer grained permissions to Viewsets
    This ensures that:
    * Anonymous users:
      * Can view public worlds
    * Logged-in users:
      * Can view public worlds
      * Can modify worlds and maps of which they are a part.
    
     TODO: allow superusers to view and edit every world.

--- backend/minemaphelper/minemap/views.py
+++ backend/minemaphelper/minemap/views.py
@@ -1,35 +1,119 @@
 from django.http import Http404
+from django.db.models import Q
+from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 from minemaphelper.minemap.models import World, MinecraftMap, MapMarker
-from minemaphelper.minemap.serializers import WorldSerializer, MinecraftMapSerializer, MapMarkerSerializer
+
+
+def user_allowed_world(world, user):
+    if world.private and user.is_anonymous or world.users.filter(pk=user.pk).count() == 0:
+        return False
+    return True
+
+
+class UserInWorld(permissions.BasePermission):
+    def has_object_permission(self, request, view, world):
+        def _has_object_permission(request, view, world):
+            if request.method in permissions.SAFE_METHODS:
+                if world.private:
+                    return user_allowed_world(world, request.user)
+                else:
+                    return True
+            else:
+                return user_allowed_world(world, request.user)
+        permission = _has_object_permission(request, view, world)
+        print(
+            f'UserInWorld.has_object_permission({request.method}, {request.user}, {world}) => {permission}')
+        return permission
+
+
+class UserAllowedMap(permissions.BasePermission):
+    def has_permission(self, request, view):
+        def _has_permission(request, view):
+            if request.method in permissions.SAFE_METHODS:
+                if view.world.private:
+                    return user_allowed_world(view.world, request.user)
+                else:
+                    return True
+            else:
+                return user_allowed_world(view.world, request.user)
+        permission = _has_permission(request, view)
+        print(
+            f'UserAllowedMap.has_permission({request.method}, {request.user}, {view.world}) => {permission}')
+        return permission
 
 
 class WorldViewSet(viewsets.ModelViewSet):
     """
-    Viewset to list, retreive and modify Worlds
+    API endpoint that allows Worlds to be viewed or edited.
     """
 
-    def get_queryset(self):
+    queryset = World.objects.all()
+
+    def filter_queryset(self, queryset):
         if self.request.user.is_anonymous:
-            return World.objects.filter(private=False)
-        return self.request.user.worlds.all()
+            return queryset.filter(private=False)
+        return queryset.filter(Q(users__in=[self.request.user]) | Q(private=False))
+
     serializer_class = WorldSerializer
     pagination_class = None
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, UserInWorld]
 
 
-class MapViewSet(viewsets.ModelViewSet):
+class HasWorldMixin:
+    """
+    Mixin that sets self.world based on the world_pk kwarg
+    """
+
+    def initial(self, *args, **kwargs):
+        try:
+            self.world = World.objects.get(pk=self.kwargs['world_pk'])
+        except World.DoesNotExist:
+            self.world = None
+        return super().initial(*args, **kwargs)
+
+
+class MapViewSet(HasWorldMixin, viewsets.ModelViewSet):
     """
     API endpoint that allows maps to be viewed or edited.
     """
+
     def get_queryset(self):
-        return MinecraftMap.objects.filter(world=self.kwargs['world_pk'])
+        if self.world is None:
+            raise Http404
+        return MinecraftMap.objects.filter(world=self.world)
+
     serializer_class = MinecraftMapSerializer
     pagination_class = None
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, UserAllowedMap]
+
+    # This is a very disgusting way to have two lookup vars but still effective
+    lookup_field = 'coordinates'
+    lookup_value_regex = '(?P<zoom>[0-9]+)/(?P<x>-?[0-9]+)/(?P<z>-?[0-9]+)'
+
+    def get_object(self):
+        """
+        Returns the object the view is displaying.
+        This is overriden to allow to fetch the zoom, x, and z parameters from the URL.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+
+        filter_kwargs = {
+            'zoom': self.kwargs['zoom'],
+            'x': self.kwargs['x'],
+            'z': self.kwargs['z']
+        }
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
     @action(
         detail=False,
@@ -66,6 +150,7 @@ class MarkerViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows markers to be views or edited.
     """
+
     def get_queryset(self):
         return MapMarker.objects.filter(world=self.kwargs['world_pk'])
     serializer_class = MapMarkerSerializer
