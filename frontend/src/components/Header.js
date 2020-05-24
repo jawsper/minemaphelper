57316c18d5e72130d13bd68cde03b6975d665cda
Jasper Seidel <code@jawsper.nl>
2020-05-24 23:18:06 +0200

    [frontend] Update Header and HeaderContainer
    Adds WorldSelection and Github link

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button, Form } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import WorldSelection from './WorldSelection';
 
-export default class Header extends Component {
+class Header extends React.Component {
+    onSearchSubmit = e => {
+        e.preventDefault();
+        e.stopPropagation();
+        this.props.handleSearch(e.target['query'].value);
+    }
+
     render() {
         const user = this.props.authentication.get('user')
-        const logged_in = this.props.authentication.get('user') !== null;
 
         let auth = null;
-        if (user !== null) {
-            auth = (
-                <Navbar.Text>
+        if (user !== null && !user.get('anonymous')) {
+            auth = [
+                <Navbar.Text key={0}>
                     Logged in as {user.get('username')}
                     {' '}
-                    <Button variant="outline-light" onClick={this.props.handleLogout}>Logout</Button>
-                </Navbar.Text>
-            );
+                </Navbar.Text>,
+                <Button key={1} className="ml-sm-2" variant="outline-light" onClick={this.props.handleLogout}>Logout</Button>
+            ];
         } else {
             auth = <Button variant="outline-light" onClick={this.props.handleLogin}>Login</Button>;
         }
@@ -22,9 +30,27 @@ export default class Header extends Component {
         return (
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand>Minemap</Navbar.Brand>
-                <Nav className="mr-auto"></Nav>
+                <Nav className="mr-auto" />
+                {/* <Form inline className="mr-sm-2" onSubmit={this.onSearchSubmit}>
+                    <Form.Control name="query" type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button type="submit">Search</Button>
+                </Form> */}
+                <Nav className="mr-auto" />
+                <WorldSelection
+                    className="mr-sm-2"
+                    current_world={this.props.current_world}
+                    worlds={this.props.worlds}
+                    onSelectWorld={this.props.onSelectWorld} />
                 {auth}
+                <Nav.Link
+                    href="https://github.com/jawsper/minemaphelper"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} size="lg" />
+                </Nav.Link>
             </Navbar>
         );
     }
 }
+
+export default Header;
--- frontend/src/containers/HeaderContainer.js
+++ frontend/src/containers/HeaderContainer.js
@@ -2,34 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Header from '../components/Header';
-import { authRequest, authCheckLogin, authLogout } from '../actions';
+import actions from '../actions';
+import { currentWorldIDSelector, worldsLoadedSelector, worldsListSelector } from '../selectors';
 
 class HeaderContainer extends Component {
-    componentDidMount() {
-        this.props.handleCheckLoggedIn();
-    }
-
     render() {
         return (
-            <Header 
+            <Header
                 handleLogin={this.props.handleLogin}
                 handleLogout={this.props.handleLogout}
-                authentication={this.props.authentication} />
+                handleSearch={this.props.handleSearch}
+                authentication={this.props.authentication}
+                current_world={this.props.current_world}
+                worlds_loaded={this.props.worlds_loaded}
+                worlds={this.props.worlds}
+                onSelectWorld={this.props.handleSelectWorld} />
         );
     }
 }
 
-function mapStateToProps(state) {
-    const authentication = state.get('authentication');
-    return { authentication };
-}
+const mapStateToProps = state => ({
+    authentication: state.get('authentication'),
+    current_world: currentWorldIDSelector(state),
+    worlds_loaded: worldsLoadedSelector(state),
+    worlds: worldsListSelector(state),
+});
 
-function mapDispatchToProps(dispatch) {
-    return {
-        handleCheckLoggedIn: () => dispatch(authCheckLogin()),
-        handleLogin: () => dispatch(authRequest()),
-        handleLogout: () => dispatch(authLogout()),
-    };
-}
+const mapDispatchToProps = (dispatch) => ({
+    handleCheckLoggedIn: () => dispatch(actions.authentication.authCheckLogin()),
+    handleLogin: () => dispatch(actions.authentication.authRequest()),
+    handleLogout: () => dispatch(actions.authentication.authLogout()),
+
+    handleSearch: (query) => { console.log('search', query) },
+
+    handleSelectWorld: (world) => dispatch(actions.worlds.selectWorld(world)),
+});
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(HeaderContainer)
