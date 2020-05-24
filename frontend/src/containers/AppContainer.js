0b3db829f7bf68abbbbf794739be2c426d3bd911
Jasper Seidel <code@jawsper.nl>
2020-05-24 23:14:32 +0200

    [frontend] Update AppContainer to show loading state until worlds loaded
    Also stops MinemapContainer from rendering if there is no World to show

--- frontend/src/containers/AppContainer.js
+++ frontend/src/containers/AppContainer.js
@@ -1,20 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import HeaderContainer from './HeaderContainer';
 import MinemapContainer from './MinemapContainer';
 import { connect } from 'react-redux';
+import Immutable from 'immutable';
 
+import actions from '../actions';
 import AuthenticationDialogContainer from './AuthenticationDialogContainer';
+import Loading from '../components/Loading';
+import { userSelector, worldsLoadedSelector, currentWorldSelector } from '../selectors';
 
-class AppContainer extends Component {
+class AppContainer extends React.Component {
+    componentDidMount() {
+        this.props.handleCheckLoggedIn();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // When user changes, load worlds
+        if (!Immutable.is(prevProps.user, this.props.user)) {
+            this.props.handleLoadWorlds();
+        }
+    }
+
     render() {
         return (
             <div>
                 <HeaderContainer />
                 <AuthenticationDialogContainer />
-                <MinemapContainer />
+                <Loading until={this.props.worlds_loaded === true} message="Loading worlds">
+                    {this.props.current_world ? <MinemapContainer /> : null}
+                </Loading>
             </div>
-        )
+        );
     }
 }
 
-export default connect()(AppContainer);
+const mapStatetoProps = (state) => {
+    const user = userSelector(state);
+    const worlds_loaded = worldsLoadedSelector(state);
+    const current_world = currentWorldSelector(state);
+    return { user, worlds_loaded, current_world };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleCheckLoggedIn: () => dispatch(actions.authentication.authCheckLogin()),
+        handleLoadWorlds: () => dispatch(actions.worlds.loadWorlds()),
+    }
+}
+
+
+export default connect(mapStatetoProps, mapDispatchToProps)(AppContainer);
